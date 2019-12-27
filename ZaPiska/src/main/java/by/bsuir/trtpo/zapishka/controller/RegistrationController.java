ce45d11feeef9e62c02e90ee9fa4937265f33f51
Dima Alhovik <halapsikon@mail.ru>
2019-12-27 20:09:32 +0300

    remove unnecery files

--- ZaPiska/src/main/java/by/bsuir/trtpo/zapishka/controller/RegistrationController.java
+++ ZaPiska/src/main/java/by/bsuir/trtpo/zapishka/controller/RegistrationController.java
@@ -3,14 +3,10 @@ package by.bsuir.trtpo.zapishka.controller;
 import by.bsuir.trtpo.zapishka.bean.Role;
 import by.bsuir.trtpo.zapishka.bean.User;
 import by.bsuir.trtpo.zapishka.dao.UserDAO;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collections;
 import java.util.Map;
